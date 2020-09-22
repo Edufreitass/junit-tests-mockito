f1ddbd6cc2eb487098f1786a46cb623d7a8acfaa
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-09-22 17:02:16 -0300

    Spring Boot Testing | Writing JUnit Tests using JUnit and Mockito

---
+++ src/main/java/com/javatechie/spring/mockito/api/JunitTestsMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.mockito.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestsMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestsMockitoApplication.class, args);
+	}
+
+}
